d6c41551c9f16afc40147bde5b952cc19a2731b3
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-04-08 01:24:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,65 @@
 'use strict';
 
-// write your code here
+const phonesUrl
+= 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+function getPhonesFromApi() {
+  return fetch(phonesUrl);
+}
+
+function timeout() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => (
+      reject(new Error('Error'))
+    ), 5000);
+  });
+}
+
+function getPhonesWithTimeout() {
+  return Promise.race([
+    getPhonesFromApi(),
+    timeout(),
+  ])
+    .then(response => {
+      return response.json();
+    });
+}
+
+function getPhones() {
+  return getPhonesWithTimeout()
+    .then(phones => {
+      const phonesIds = phones.map(phone => phone.id);
+
+      return phonesIds;
+    });
+}
+
+function getDetails(ids) {
+  const detailsList = ids.map(id => {
+    return fetch(
+      // eslint-disable-next-line max-len
+      `https://mate-academy.github.io/phone-catalogue-static/api/phones/${id}.json`
+    )
+      .then(response => response.json());
+  });
+
+  return Promise.all(detailsList);
+}
+
+function createList(detailsList) {
+  const list = document.createElement('ul');
+
+  detailsList.map(element => {
+    const li = document.createElement('li');
+
+    li.textContent = element.name;
+
+    list.append(li);
+  });
+
+  document.body.append(list);
+};
+
+getPhones(phonesUrl)
+  .then(getDetails)
+  .then(createList);
